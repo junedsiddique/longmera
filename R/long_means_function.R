fe10fe06ef3fa46c01ce3978ecb8c704a9ceb0b5
junedsiddique <siddique@northwestern.edu>
2025-09-23 15:02:37 -0500

    cleaned up/updated documentation

--- R/long_means_function.R
+++ R/long_means_function.R
@@ -5,7 +5,7 @@
 #' GLMMadaptive package or an object of type "geeglm" that is obtained
 #' from the gee package.
 #' @param tx.contrast0 A vector specifying fixed values of covariates and variables
-#' to be averaged over when (NA) calculating a margin for treatment group at baseline.
+#' to be averaged over (indicated by NA) when calculating a margin for treatment group at baseline.
 #' Must be the same length as the coefficient vector from the object model.
 #' @param tx.contrast1 A vector to calculate the margin for the treatment group at follow-up.
 #' @param ctrl.contrast0 A vector to calculate the margin for the control group at baseline.
@@ -21,16 +21,15 @@
 #' # Fit a mixed-effects model using the GLMMadaptive backage
 #' library(GLMMadaptive)
 #' fm1 <- mixed_model(fixed = quit ~ group + time + grouptime + race + tv + manual,
-#' random = ~ time | id, data = gruder,
-#' family = binomial())
-
+#'                    random = ~ time | id, data = gruder, family = binomial())
+#'
 #' # Specify contrasts for means
 #' tx.contrast0=c(1, 1, 0, 0, NA, NA, NA) # tx time
 #' tx.contrast1=c(1, 1, 4, 4, NA, NA, NA) # tx time 4
-
+#'
 #' ctrl.contrast0=c(1, 0, 0, 0, NA, NA, NA) #ctrl time 0
 #' ctrl.contrast1=c(1, 0, 4, 0, NA, NA, NA) #ctrl time 4
-
+#'
 #' # Enter model object and contrasts into the long_means function
 #' output <- long_means(fm1, tx.contrast0=tx.contrast0, tx.contrast1=tx.contrast1,
 #'                     ctrl.contrast0=ctrl.contrast0, ctrl.contrast1=ctrl.contrast1)
@@ -39,7 +38,7 @@
 #' if (requireNamespace("geepack", quietly = TRUE)) {
 #' library(geepack)
 #' mod1 <- geeglm(quit ~ group + time + grouptime + race + tv + manual, id=id,
-#' data=gruder, corstr="unstructured", family=binomial())
+#'                       data=gruder, corstr="unstructured", family=binomial())
 #'
 #' output <- long_means(fm1, tx.contrast0=tx.contrast0, tx.contrast1=tx.contrast1,
 #'                     ctrl.contrast0=ctrl.contrast0, ctrl.contrast1=ctrl.contrast1)
